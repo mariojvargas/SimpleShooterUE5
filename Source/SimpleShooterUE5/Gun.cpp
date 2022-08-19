8cb4dcb2184994a8192578e86738867e1797a8fc
Mario Vargas <angstrey@hotmail.com>
2022-08-19 04:33:05 -0400

    Add Rifle skeletal mesh to Rifle blueprint

--- Source/SimpleShooterUE5/Gun.cpp
+++ Source/SimpleShooterUE5/Gun.cpp
@@ -2,6 +2,7 @@
 
 
 #include "Gun.h"
+#include "Components/SkeletalMeshComponent.h"
 
 // Sets default values
 AGun::AGun()
@@ -9,6 +10,11 @@ AGun::AGun()
  	// Set this actor to call Tick() every frame.  You can turn this off to improve performance if you don't need it.
 	PrimaryActorTick.bCanEverTick = true;
 
+	RootSceneComponent = CreateDefaultSubobject<USceneComponent>(TEXT("Root Scene Component"));
+	SetRootComponent(RootSceneComponent);
+
+	SkeletalMesh = CreateDefaultSubobject<USkeletalMeshComponent>(TEXT("Gun Skeletal Mesh"));
+	SkeletalMesh->SetupAttachment(RootSceneComponent);
 }
 
 // Called when the game starts or when spawned
