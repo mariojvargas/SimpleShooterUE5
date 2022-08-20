1e950a4ca314c24e119804dd6db61bb2df8e0f01
Mario Vargas <angstrey@hotmail.com>
2022-08-20 14:39:52 -0400

    Prevent shooting if character is dead

--- Source/SimpleShooterUE5/ShooterCharacter.cpp
+++ Source/SimpleShooterUE5/ShooterCharacter.cpp
@@ -3,6 +3,7 @@
 
 #include "ShooterCharacter.h"
 #include "Gun.h"
+#include "Components/CapsuleComponent.h"
 
 // Sets default values
 AShooterCharacter::AShooterCharacter()
@@ -84,6 +85,12 @@ float AShooterCharacter::TakeDamage(float DamageAmount, struct FDamageEvent cons
 	Health -= DamageToApply;
 	
 	UE_LOG(LogTemp, Warning, TEXT("Damage Taken! Health [%f]"), Health);
+
+	if (IsDead())
+	{
+		DetachFromControllerPendingDestroy();
+		GetCapsuleComponent()->SetCollisionEnabled(ECollisionEnabled::NoCollision);
+	}
 
 	return DamageToApply;
 }
