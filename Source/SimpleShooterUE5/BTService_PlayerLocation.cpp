ab97b3a1f2a95a2243554f02fa0bf2c927acd38c
Mario Vargas <angstrey@hotmail.com>
2022-08-20 20:33:11 -0400

    Add ability to aim up or down

--- Source/SimpleShooterUE5/BTService_PlayerLocation.cpp
+++ Source/SimpleShooterUE5/BTService_PlayerLocation.cpp
@@ -20,8 +20,5 @@ void UBTService_PlayerLocation::TickNode(UBehaviorTreeComponent& OwnerComp, uint
         return;
     }
 
-    OwnerComp.GetBlackboardComponent()->SetValueAsVector(
-        GetSelectedBlackboardKey(), 
-        PlayerPawn->GetActorLocation()
-    );
+    OwnerComp.GetBlackboardComponent()->SetValueAsObject(GetSelectedBlackboardKey(), PlayerPawn);
 }
--- Source/SimpleShooterUE5/BTService_PlayerLocationIfSeen.cpp
+++ Source/SimpleShooterUE5/BTService_PlayerLocationIfSeen.cpp
@@ -30,7 +30,7 @@ void UBTService_PlayerLocationIfSeen::TickNode(UBehaviorTreeComponent& OwnerComp
 
     if (OwnerAIController->LineOfSightTo(PlayerPawn))
     {
-        OwnerComp.GetBlackboardComponent()->SetValueAsVector(GetSelectedBlackboardKey(), PlayerPawn->GetActorLocation());
+        OwnerComp.GetBlackboardComponent()->SetValueAsObject(GetSelectedBlackboardKey(), PlayerPawn);
     }
     else
     {
